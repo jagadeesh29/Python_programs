ecdd4c5921bf8f37ca690c3f1d586e4c7e806612
jagadeesh <36129585+jagadeesh29@users.noreply.github.com>
2018-12-08 03:37:05 +0530

    Add files via upload

---
+++ 14.Length_of_string.py
@@ -0,0 +1,10 @@
+#14. Given a string, find the length of the string and print it to the console
+
+def len_of_string():
+
+    user_input = input("enter the string: ")
+
+    result = len(user_input)
+    print(result)
+
+len_of_string()
