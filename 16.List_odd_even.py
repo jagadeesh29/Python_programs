d43e8df321e8d73ad4a9c473eb710440750e54cf
jagadeesh <36129585+jagadeesh29@users.noreply.github.com>
2018-12-14 16:36:28 +0530

    Update 16.List_odd_even.py

--- 16.List_odd_even.py
+++ 16.List_odd_even.py
@@ -15,3 +15,16 @@ def list_oddfirst_evensecond():
 
 ans = list_oddfirst_evensecond()
 print(ans)
+
+
+'''
+Testcases:
+
+Input                                                   output
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]               [1, 3, 5, 7, 9, 2, 4, 6, 8, 10]
+[1, 2, 3,321, 4, 5, 6, 7, 8, 9,10,10]         [1, 3, 321, 5, 7, 9, 2, 4, 6, 8, 10, 10]
+[a,b,c]                                       TypeError: not all arguments converted during string formatting       
+[@]                                           TypeError: not all arguments converted during string formatting
+[-1,0,0,-4, 2, 3, 4, 5, 6, 7, 8, 9, 10]       [-1, 3, 5, 7, 9, 0, 0, -4, 2, 4, 6, 8, 10]
+[-1.5,0.5,0,-4, 2, 3, 4, 5, 6, 7, 8, 9, 10]   [-1.5, 0.5, 3, 5, 7, 9, 0, -4, 2, 4, 6, 8, 10]
+'''
