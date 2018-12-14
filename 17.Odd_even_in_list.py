46463f28ca1306d17882fca93db5ef8af98c3930
jagadeesh <36129585+jagadeesh29@users.noreply.github.com>
2018-12-14 16:47:10 +0530

    Update 17.Odd_even_in_list.py

--- 17.Odd_even_in_list.py
+++ 17.Odd_even_in_list.py
@@ -14,3 +14,15 @@ def odd_even_inlist(lis):
 
 ans = odd_even_inlist(list1)
 print(ans)
+
+'''
+TESTCASES:
+        INPUT                                                       OUTPUT
+    1. [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]                               (25, 30)
+    2. [-3,2,5,6,3]                                                  (5, 8)
+    3. [-3,2,5,6,3,-15,-10]                                          (-10, -2)
+    4. [0,0,0]                                                       (0,0)
+    
+    5.  [-0,0,'a']                                                   TypeError: not all arguments converted during string formatting
+    
+    '''
